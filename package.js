fab4b2d564ba9da8fbda50d377b565beca2f187c
Abigail Watson <awatson1978@gmail.com>
2017-03-31 17:56:21 -0500

    DTSU3 datatypes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-audit-event',
-  version: '1.3.1',
+  version: '1.3.2',
   summary: 'HL7 FHIR Resource - AuditEvent',
   git: 'https://github.com/clinical-meteor/hl7-resource-audit-event',
   documentation: 'README.md'
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
   api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.8');
+  api.use('clinical:hl7-resource-datatypes@3.0.0');
   api.use('clinical:hl7-resource-bundle@1.3.10');
   api.use('clinical:hl7-resource-binary@1.0.5');
 
