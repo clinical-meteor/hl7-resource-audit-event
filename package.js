6591b119c3e7e011b171751ca6b1ec7ba020592d
Abigail Watson <abigailwatson@Abigails-Mac-mini-377974.local>
2018-07-09 00:00:11 -0500

    default to local collection (HIPAA compliant; but annoying to use)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-audit-event',
-  version: '1.4.5',
+  version: '1.4.7',
   summary: 'HL7 FHIR Resource - AuditEvent',
   git: 'https://github.com/clinical-meteor/hl7-resource-audit-event',
   documentation: 'README.md'
